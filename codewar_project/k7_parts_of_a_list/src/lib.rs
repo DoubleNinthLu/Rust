fda97d720ed7dc1df4e61c7ace6012e1ed61751a
alluc <lucy_lc@outlook.com>
2021-06-11 22:01:14 +0800

    day5

---
+++ codewar_project/k7_parts_of_a_list/src/lib.rs
@@ -0,0 +1,37 @@
+// day5 2021年6月11日
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn dotest(arr: Vec<&str>, exp: &str) -> () {
+        println!("arr: {:?}", arr);
+        let ans = part_list(arr);
+        println!("actual:\n{}", ans);
+        println!("expect:\n{}", exp);
+        println!("{}", ans == exp);
+        assert_eq!(ans, exp);
+        println!("{}", "-");
+    }
+
+    #[test]
+    fn basis_tests() {
+        dotest(vec!["I", "wish", "I", "hadn't", "come"],
+                "(I, wish I hadn't come)(I wish, I hadn't come)(I wish I, hadn't come)(I wish I hadn't, come)");
+        dotest(
+            vec!["cdIw", "tzIy", "xDu", "rThG"],
+            "(cdIw, tzIy xDu rThG)(cdIw tzIy, xDu rThG)(cdIw tzIy xDu, rThG)",
+        );
+    }
+}
+
+fn part_list(arr: Vec<&str>) -> String {
+    let mut res = String::new();
+    for i in 0..(arr.len() - 1) {
+        res.push_str(&format!(
+            "({}, {})",
+            arr[..=i].join(" "),
+            arr[(i+1)..].join(" ")
+        ))
+    }
+    res
+}
---
+++ codewar_project/k7_thinkful_string_drills_repeater/src/lib.rs
@@ -0,0 +1,21 @@
+// day5 2021年6月11日
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn basic_test() {
+        assert_eq!(repeater("a", 5), "aaaaa");
+        assert_eq!(repeater("Na", 16), "NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa");
+        assert_eq!(repeater("Wub ", 6), "Wub Wub Wub Wub Wub Wub ");
+    }
+}
+
+
+fn repeater(string: &str, n: u32) -> String {
+    let mut res = String::new();
+    for _ in 0..n {
+        res.push_str(string);
+    }
+    res
+}
